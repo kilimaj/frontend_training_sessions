21769f8de556be36d3d037e6727cb6516ecc7cb0
John Kilima <jkbundala@gmail.com>
2023-09-07 13:21:59 +0300

    Javascript data types and variable concatinations

---
+++ day4/script.js
@@ -0,0 +1,22 @@
+document.write("Hello World!");
+
+var a = 10;
+let b = 10.099;
+let c = "10";
+let d = true;
+let e = { a: 1 };
+
+console.log(a);
+console.log(b);
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof e);
+
+console.log(`${a} ${b} ${c} ${d} ${e}`);
+console.log(a + " " + b + " " + c + " " + d);
+
+let f = `${a}---${b}---${c}`;
+console.log(f);
