a44682696a77f175a4418dee82a39d4bc50ee95b
John Kilima <jkbundala@gmail.com>
2023-09-08 10:40:53 +0300

    Going on with loops

---
+++ day5/script.js
@@ -0,0 +1,47 @@
+// for (var i = 100; i >= 0; i--) {
+//   if (i > 80) continue;
+//   console.log(i);
+//   if (i == 50) break;
+// }
+
+// let sum = 0;
+// for (var i = 100; i >= 0; i--) {
+//   if (i % 2 === 1) continue;
+//   // When its not an even number it continues the loop and only executes
+//   // when its even
+//   sum = sum + i;
+// }
+// console.log(sum);
+
+// var sum = 0;
+// for (var i = 1; i <= 100; i++) {
+//   sum = sum + i;
+//   if (sum > 4000) {
+//     console.log(i, sum);
+//     break;
+//   }
+// }
+
+// var i = 0;
+// do {
+//   console.log(i);
+//   i++;
+// } while (i <= 100);
+
+// var i = 1000;
+// do {
+//   console.log(i);
+//   i++;
+// } while (i <= 100);
+
+// var i = 1;
+// do {
+//   i++;
+//   console.log(i);
+// //   if (i < 50) continue;
+//   if (i > 50) break;
+// } while (i <= 100);
+
+// Times table
+
+
