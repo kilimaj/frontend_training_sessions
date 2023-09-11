2c93f7028f1eb7f7d0e2c629e0e175c6873c2426
John Kilima <jkbundala@gmail.com>
2023-09-11 11:16:15 +0300

    Event listening quiz

--- day6/quiz.js
+++ day6/quiz.js
@@ -13,6 +13,30 @@ document.querySelector(".box").addEventListener("mouseover", function (event) {
   } else {
     this.innerHTML = "LEFT";
   }
-  this.style.backgroundColor = "#" + 
-  parseInt(event.offsetX * 50000, 16);
+  this.style.backgroundColor = "#" + parseInt(event.offsetX * 50000, 16);
 });
+
+function funA() {
+  console.log("fun-a");
+}
+
+function funB() {
+  console.log("fun-b");
+}
+
+document.querySelector(".box-200").addEventListener("click", funA);
+
+document.querySelector(".box-200").addEventListener("click", funB);
+
+document.querySelector(".box-200").removeEventListener("click", funA);
+
+for (let i = 0; i < 100; i++) {
+  let box = document.createElement("div");
+  box.className = "box-100";
+  box.innerHTML = "BOX-" + i;
+  document.body.appendChild(box);
+
+  box.addEventListener("click", function () {
+    console.log(i);
+  });
+}
