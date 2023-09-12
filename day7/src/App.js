5481c7280087c888189b3f6cb780184aa3f97f5b
John Kilima <jkbundala@gmail.com>
2023-09-12 11:53:03 +0300

    git basics

--- day7/src/App.js
+++ day7/src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+// function App() {
+// class App () {}
+const App = () => {
+  let a = "AAA";
+  let b = <div>BBBB</div>;
+  const fun = (param) => {
+    return "function" + param;
+  };
+
+  const funA = (param) => {
+    return <div>{param}</div>;
+  };
+
+  const click = () => {
+    console.log("click...");
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* Comment */}
+      {funA("A")}
+      {b}
+      {fun("+param")}
+      {fun(<div>AAAA</div>)}
+
+      {fun("<div>AAAA</div>")}
+
+      <div
+        onClick={({ target }) => {
+          console.log(target);
+        }}
+        className={a}
+      >
+        {a}
+      </div>
+      <div
+        className={a}
+        onClick={({ target, type }) => {
+          console.log(target, type);
+        }}
+      >
+        {a}
+      </div>
+      <div
+        className={1 + 2}
+        onClick={() => {
+          click();
+        }}
+      >
+        CLICK
+      </div>
+      <div className={a + 2}>{a}</div>
+
+      <div>HELLO WORLD !</div>
+    </>
   );
-}
+};
 
 export default App;
