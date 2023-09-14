25c12391816395bd8363052f0e4d6be9d4979a81
John Kilima <jkbundala@gmail.com>
2023-09-14 09:28:41 +0300

    Data.js file

---
+++ day9/src/components/Data.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const JsonServer = () => {
+  const loadData = async () => {
+    const response = await axios.get("http://localhost:4000/posts");
+    console.log(response.data);
+  };
+
+  return (
+    <>
+      <button
+        onClick={() => {
+          loadData();
+        }}
+      >
+        GET
+      </button>
+    </>
+  );
+};
