7a15b47723b5c232314f9fcce283616f3f065905
John Kilima <jkbundala@gmail.com>
2023-09-14 11:01:30 +0300

    Delete daa and display get data on screen

--- day9/src/components/Data.js
+++ day9/src/components/Data.js
@@ -1,10 +1,13 @@
 import axios from "axios";
+import React, { useState } from "react";
 
 const JsonServer = () => {
+  const [data, setData] = useState([]);
   const loadData = async () => {
     const response = await axios.get("http://localhost:4000/posts");
     console.log(response);
     console.log(response.data);
+    setData(response.data);
   };
 
   const addData = async () => {
@@ -27,8 +30,28 @@ const JsonServer = () => {
     console.log(response.data);
   };
 
+  const deleteData = async () => {
+    const response = await axios.delete("http://localhost:4000/posts/2");
+    console.log(response);
+    console.log(response.data);
+  };
+
   return (
     <>
+      <table border="1">
+        <tbody>
+          {data &&
+            data.map((item, index) => {
+              return (
+                <tr key={index}>
+                  <td>{index}</td>
+                  <td>{item.title}</td>
+                  <td>{item.author}</td>
+                </tr>
+              );
+            })}
+        </tbody>
+      </table>
       <button
         onClick={() => {
           loadData();
@@ -51,6 +74,15 @@ const JsonServer = () => {
         }}
       >
         UPDATE
+      </button>
+
+      <br></br>
+      <button
+        onClick={() => {
+          deleteData();
+        }}
+      >
+        DELETE
       </button>
     </>
   );
